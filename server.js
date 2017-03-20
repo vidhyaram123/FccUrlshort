ea83274377688d566f77abfa2a1bb7f26281c856
vids <vidhya_rnath@yahoo.com>
2017-03-20 19:41:28 +0000

    bug correction

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get('/',function(req,res){
 })
 
 app.post('/new',function(req,res){
-    console.log("url request"+ req.body);
+    //console.log("url request"+ req.body);
     var urll = String(req.body.longurl);
     checkAndInsert(urll,res);
 })
